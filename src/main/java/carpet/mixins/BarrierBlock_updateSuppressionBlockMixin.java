f12b2d819d4b6b8580ab5229d096cdd583e6bd80
Oliver <59418903+Oliver-makes-code@users.noreply.github.com>
2022-03-11 22:10:14 -0600

    Update BarrierBlock_updateSuppressionBlockMixin.java

--- src/main/java/carpet/mixins/BarrierBlock_updateSuppressionBlockMixin.java
+++ src/main/java/carpet/mixins/BarrierBlock_updateSuppressionBlockMixin.java
@@ -9,13 +9,13 @@ import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BarrierBlock;
+import net.minecraft.world.level.block.BeaconBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.PoweredRailBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
-@Mixin(BarrierBlock.class)
+@Mixin(BeaconBlock.class)
 public class BarrierBlock_updateSuppressionBlockMixin extends Block {
     private boolean shouldPower = false;
 
